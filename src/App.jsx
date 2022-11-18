cd2df5ef7c61237525f4357945d179355d1c332f
sglkc <31957516+sglkc@users.noreply.github.com>
2022-11-18 10:33:25 +0700

    feat: responsive breakpoint with @react-hook/window-size
    build: add @react-hook/window-size
    chore: use main.jsx as main entry

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { useWindowWidth } from '@react-hook/window-size/throttled';
 import Router from '@/router/Router.jsx';
 import store from '@/store';
 import '@fontsource/nunito/variable.css';
 import './App.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const root = document.getElementById('root').classList;
+const breakpoints = {
+  576: 'sm',
+  768: 'md',
+  992: 'lg'
+};
+
+export default function App() {
+  const width = useWindowWidth({ fps: 15 });
+
+  for (const px in breakpoints) {
+    if (width <= px)
+      root.add(breakpoints[px])
+    else
+      root.remove(breakpoints[px]);
+  }
+
+  return (
     <Provider store={store}>
       <Router />
     </Provider>
-  </React.StrictMode>
-);
+  );
+}
